52073e5544cbf1d16455caaa0344c4a75118b2e0
Danila Rusak <skinny4550@gmail.com>
2024-07-09 23:04:22 +0200

    initial commit

---
+++ distributedurl_shortener/src/test/java/com/example/distributedurl_shortener/DistributedurlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributedurl_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedurlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
